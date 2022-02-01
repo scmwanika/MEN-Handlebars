49672096daa36e3a602599ffc3ff86bf66486e58
Stephen Mwanika <stephen.mwanika@kanzucode.com>
2022-02-01 19:26:00 +0300

    updated

--- static/js/run_app.js
+++ static/js/run_app.js
@@ -1,6 +1,8 @@
 const supplierName = () => {
   // field supplied_by == field supplier_name
   document.getElementById('supplied_by').value = document.getElementById('supplier_name').value;
+  // insert date automatically
+  document.getElementById('created_at').value = Date();
 }
 
 const supplierForm = () => {
