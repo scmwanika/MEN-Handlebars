df9b2b206396fb8ebc245d13a7a6d0c7726534fa
Stephen Mwanika <stephen.mwanika@kanzucode.com>
2022-04-05 23:16:51 +0300

    updated

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const app = require('express')();
+const express = require('express');
 
 const PORT = process.env.PORT || 3000
 
-app.get('', (req, res) => {
-  res.send('Hello World');
+const app = express();
+
+// HANDLEBARS THE VIEW TEMPLATE ENGINE
+app.set('view engine', 'hbs');
+
+// SERVING MULTIPLE STATIC DIRECTORIES WITH MIDDLEWARE FUNCTION express.static
+app.use(express.static('static/css'));
+app.use(express.static('static/img'));
+app.use(express.static('uploads'));
+
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
 app.listen(PORT, () => {
